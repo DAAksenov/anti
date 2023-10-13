0bf0560518fe7e5e175311f9ceaa97d10a73c09a
TheSuspect17 <shipilovdima2002@yandex.ru>
2023-10-13 21:51:38 +0300

    qwe

--- antipositive/antipositive/settings.py
+++ antipositive/antipositive/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'users'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'antipositive.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates' ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,9 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/'
--- antipositive/main/views.py
+++ antipositive/main/views.py
@@ -1,28 +1,62 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'title': 'Beautiful is better than ugly',
-        'author': 'John Doe',
-        'content': 'Beautiful is better than ugly',
-        'published_at': 'October 1, 2022'
-    },
-    {
-        'title': 'Explicit is better than implicit',
-        'author': 'Jane Doe',
-        'content': 'Explicit is better than implicit',
-        'published_at': 'October 1, 2022'
-    }
-]
-
+from django.http import HttpResponse
 
 def home(request):
-    context = {
-        'posts': posts,
-        'title': 'Zen of Python'
-    }
-    return render(request, 'blog/home.html', context)
+    return render(request, 'home.html')
 
 
-def about(request):
-    return render(request, 'blog/about.html')
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm  # Import your custom form
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')  # Replace 'home' with the URL name of your home page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Replace 'home' with the URL name of your home page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.shortcuts import render, redirect
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # To keep the user logged in
+            return redirect('password_change_done')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change-password/change_password.html', {'form': form})
+
+def password_change_done(request):
+    return render(request, 'change-password/password_change_done.html')
